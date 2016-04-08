28305b68b63700c62efb2ced4b447ed7d38a5a38
yukan <yuk@cairenhui.com>
2016-04-08 09:47:49 +0800

    方法改进

--- source/js/placeholderIE.js
+++ source/js/placeholderIE.js
@@ -1,59 +1,69 @@
 //ie
 function placeHolderIE(){
-    $('input[placeholder],textarea[placeholder]').each(function(index, el) {
-        var that = this;
-        var holderName = $(this).attr('placeholder');
-        var _placeTemp = $('<div class="placeTemp">'+ holderName +'<div>');
-        var inputPadLeft = $(this).css('paddingLeft');
-        var inputPadTop = $(this).css('paddingTop');
-        var inputHeight = $(this).css('height');
-        var inputLineHeight = $(this).css('lineHeight');
-        _placeTemp.insertAfter($(this)).css({
-            'lineHeight': inputLineHeight,
-            'height': inputHeight
-        });
-        _placeTemp.pin({
-            base: $(that),
-            selfXY: [0, 0],
-            baseXY: [inputPadLeft, inputPadTop]
-        })
-
-        _placeTemp.click(function(event) {
-            console.info(123)
-            $(this).hide();
-            $(that).focus();
-        });
-
-        if ($(this).val() != '') {
-            _placeTemp.hide();
-        };
-
-        $(this).focus(function(event) {
-            $(this).siblings('.placeTemp').hide();
-        });
-        $(this).keyup(function(event) {
-            var that = this
-            //若前后有空格
-            if ($.trim($(that).val()) == '') {
-                $(this).siblings('.placeTemp').show();
+    var hasPlaceholderSupport = function(){
+        var attr = "placeholder";
+        var input = document.createElement('input');
+        return attr in input;
+    }
+    var support = hasPlaceholderSupport();
+    if(!support){
+        $('input[placeholder],textarea[placeholder]').each(function(index, el) {
+            var that = this;
+            var holderName = $(this).attr('placeholder');
+            var _placeTemp = $('<div class="placeTemp">'+ holderName +'<div>');
+            var inputPadLeft = $(this).css('paddingLeft');
+            var inputPadTop = $(this).css('paddingTop');
+            var inputHeight = $(this).css('height');
+            var inputLineHeight = $(this).css('lineHeight');
+            if($(this).next().hasClass('placeTemp')) {
+                $(this).next().remove();
             }
-            else {
-                $(this).siblings('.placeTemp').hide();
+            _placeTemp.insertAfter($(this)).css({
+                'lineHeight': inputLineHeight,
+                'height': inputHeight
+            });
+            _placeTemp.pin({
+                base: $(that),
+                selfXY: [0, 0],
+                baseXY: [inputPadLeft, inputPadTop]
+            })
+
+            _placeTemp.click(function(event) {
+                $(this).hide();
+                $(that).focus();
+            });
+
+            if ($(this).val() != '') {
+                _placeTemp.hide();
             };
-            //tab输入
-            if (event.keyCode == 9) {   
-                $(this).trigger('focus');
-            }else{
+
+            $(this).focus(function(event) {
+                $(this).siblings('.placeTemp').hide();
+            });
+            $(this).keyup(function(event) {
+                var that = this
+                //若前后有空格
                 if ($.trim($(that).val()) == '') {
                     $(this).siblings('.placeTemp').show();
                 }
                 else {
                     $(this).siblings('.placeTemp').hide();
                 };
-            };
+                //tab输入
+                if (event.keyCode == 9) {   
+                    $(this).trigger('focus');
+                }else{
+                    if ($.trim($(that).val()) == '') {
+                        $(this).siblings('.placeTemp').show();
+                    }
+                    else {
+                        $(this).siblings('.placeTemp').hide();
+                    };
+                };
+            });
+            $(this).blur(function(event) {
+                $(this).trigger('keyup');
+            });
         });
-        $(this).blur(function(event) {
-            $(this).trigger('keyup');
-        });
-    });
+    }
 }
